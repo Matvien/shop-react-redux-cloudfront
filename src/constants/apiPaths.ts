a14f149b78ddb738d1ac5231ab6f6543f39dbe62
Evgenii Matvienko <evgenii_matvienko@epam.com>
2024-04-05 09:53:23 +0300

    update bff link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://tt6yk38ak1.execute-api.eu-central-1.amazonaws.com",
+  bff: "https://9w25sbrllh.execute-api.eu-central-1.amazonaws.com/",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
